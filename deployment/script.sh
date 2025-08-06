b152e0242e98e74dd577cb9d72e4060913bac1e1
tiboeycken <tibo.eycken@gmail.com>
2025-08-07 00:55:23 +0200

    Feat (WIP): Script is complete, needs some cleaning up

--- deployment/script.sh
+++ deployment/script.sh
@@ -4,12 +4,6 @@ PROJECT_ID=""
 BILLING_ACCOUNT_ID=""
 REGION="europe-west1"
 
-APIS_TO_ENABLE=(
-    cloudbuild.googleapis.com
-    run.googleapis.com
-    secretmanager.googleapis.com
-)
-
 check_gcloud_installed() {
     if ! command -v gcloud &>/dev/null; then
         echo "Error: Google Cloud CLI (gcloud) is not installed or not in your PATH."
@@ -20,10 +14,9 @@ check_gcloud_installed() {
 
 choose_project() {
     # Get array of project IDs
-    IFS=$'\n' 
-    read -d '' -r -a PROJECTS <<< "$(gcloud projects list --format="get(projectId)")"
-    i=0
+    IFS=$'\n' read -r -d '' -a PROJECTS < <(gcloud projects list --format="get(projectId)" && printf '\0')
     echo "Choose an existing project or create a new one:"
+    local i=0
 
     # Loop through existing projects
     for PROJECT in "${PROJECTS[@]}"; do
@@ -41,22 +34,22 @@ choose_project() {
     if [[ $INDEX -eq $i ]]; then
         new_project
     else
-        PROJECT_ID=${PROJECTS[$INDEX]}
-        gcloud config set project $PROJECT_ID
+        PROJECT_ID=$(echo "${PROJECTS[$INDEX]}" | tr -d '\r\n')
+        gcloud config set project "$PROJECT_ID"
     fi
 }
 
 new_project() {
-    read -p "Project name: " PROJECT_NAME
-#    gcloud projects create "$PROJECT_NAME"
+    read -p "Project name:" PROJECT_NAME
+    gcloud projects create "$PROJECT_NAME"
     PROJECT_ID="$PROJECT_NAME"
     echo "Project created: $PROJECT_ID"
     gcloud config set project $PROJECT_ID
 }
 
 choose_billing_account() {
-    # Get array of project IDs
-    local IFS=$'\n' 
+    # Get array of billing account names
+    IFS=$'\n' 
     read -d '' -r -a ACCOUNTS <<< "$(gcloud billing accounts list --format="get(displayName)")"
     read -d '' -r -a ACCOUNT_IDS <<< "$(gcloud billing accounts list --format="get(name)")"
 
@@ -78,7 +71,7 @@ choose_billing_account() {
         exit 1
     fi
 
-    BILLING_ACCOUNT_ID=${ACCOUNT_IDS[$INDEX]}
+    BILLING_ACCOUNT_ID=$(echo "${ACCOUNT_IDS[$INDEX]}" | tr -d '\r\n')
     link_billing_to_project
 }
 
@@ -102,45 +95,85 @@ create_and_set_custom_role(){
     local SERVICE_ACCOUNT_EMAIL="service-${PROJECT_NUMBER}@gcp-sa-cloudbuild.iam.gserviceaccount.com"
 
     # Enable required APIs
-    gcloud services enable secretmanager.googleapis.com cloudbuild.googleapis.com 
+    gcloud services enable secretmanager.googleapis.com cloudbuild.googleapis.com run.googleapis.com
 
-    sleep 10
+    # CUSTOMROLEGCPSA="customRoleGCPSA"
+    # CUSTOMROLECB="customeRoleCB"
 
-    CUSTOMROLEGCPSA="customRoleGCPSA"
-    CUSTOMROLECB="customeRoleCB"
-    gcloud iam roles create $CUSTOMROLEGCPSA \
-        --project="$PROJECT_ID" \
-        --title="Custom Role for GCP SA Service Account" \
-        --permissions="secretmanager.secrets.create,secretmanager.secrets.setIamPolicy" \
-        --stage=GA
+    # gcloud iam roles create $CUSTOMROLEGCPSA \
+    #     --project="$PROJECT_ID" \
+    #     --title="Custom Role for GCP SA Service Account" \
+    #     --permissions="secretmanager.secrets.create,secretmanager.secrets.setIamPolicy,secretmanager.secrets.getIamPolicy" \
+    #     --stage=GA
 
-    gcloud iam roles create $CUSTOMROLECB \
-        --project="$PROJECT_ID" \
-        --title="Custom Role for CB Service Account" \
-        --permissions="run.admin,secretmanager.versions.add" \
-        --stage=GA
+    # gcloud iam roles create $CUSTOMROLECB \
+    #     --project="$PROJECT_ID" \
+    #     --title="Custom Role for CB Service Account" \
+    #     --permissions="secretmanager.versions.add" \
+    #     --stage=GA
 
-    gcloud projects add-iam-policy-binding "$PROJECT_ID" \
-            --member="serviceAccount:service-${PROJECT_NUMBER}@gcp-sa-cloudbuild.iam.gserviceaccount.com" \
-            --role="projects/$PROJECT_ID/roles/$CUSTOMROLEGCPSA" 2>&1 >/dev/null
+    # Custom SA used by the Build Trigger - used to build and deploy Images to Cloud Build
+    gcloud iam service-accounts create cloud-build-deployer \
+        --description="SA for Cloud Build deployments" \
+        --display-name="Cloud Build Deployer"
 
-    gcloud projects add-iam-policy-binding "$PROJECT_ID" \
-        --member="serviceAccount:${PROJECT_NUMBER}@cloudbuild.gserviceaccount.com" \
-        --role="projects/$PROJECT_ID/roles/$CUSTOMROLECB" 2>&1 >/dev/null
+    gcloud projects add-iam-policy-binding $PROJECT_ID \
+        --member="serviceAccount:cloud-build-deployer@$PROJECT_ID.iam.gserviceaccount.com" \
+        --role="roles/run.admin"
 
-    gcloud projects add-iam-policy-binding "$PROJECT_ID" \
-        --member="serviceAccount:${PROJECT_NUMBER}@cloudbuild.gserviceaccount.com" \
+    gcloud projects add-iam-policy-binding $PROJECT_ID \
+        --member="serviceAccount:cloud-build-deployer@$PROJECT_ID.iam.gserviceaccount.com" \
         --role="roles/iam.serviceAccountUser"
 
+    gcloud projects add-iam-policy-binding $PROJECT_ID \
+        --member="serviceAccount:cloud-build-deployer@$PROJECT_ID.iam.gserviceaccount.com" \
+        --role="roles/logging.logWriter"
+
+    gcloud projects add-iam-policy-binding $PROJECT_ID \
+        --member="serviceAccount:cloud-build-deployer@$PROJECT_ID.iam.gserviceaccount.com" \
+        --role="roles/storage.admin"
+
+    gcloud projects add-iam-policy-binding $PROJECT_ID \
+        --member="serviceAccount:cloud-build-deployer@$PROJECT_ID.iam.gserviceaccount.com" \
+        --role="roles/cloudbuild.builds.builder"
+
+    # gcloud projects add-iam-policy-binding $PROJECT_ID \
+    #     --member="serviceAccount:cloud-build-deployer@$PROJECT_ID.iam.gserviceaccount.com" \
+    #     --role="roles/serviceusage.serviceUsageConsumer"
+
+    # Needed to create the github connection - needs secretmanager.secrets.create - maybe more
     gcloud projects add-iam-policy-binding "$PROJECT_ID" \
-        --member="serviceAccount:${PROJECT_NUMBER}@cloudbuild.gserviceaccount.com" \
-        --role="roles/cloudbuild.builds.editor"
+            --member="serviceAccount:service-${PROJECT_NUMBER}@gcp-sa-cloudbuild.iam.gserviceaccount.com" \
+            --role="roles/secretmanager.admin" 2>&1 >/dev/null
+
+    #     gcloud projects add-iam-policy-binding "$PROJECT_ID" \
+    #         --member="serviceAccount:service-${PROJECT_NUMBER}@gcp-sa-cloudbuild.iam.gserviceaccount.com" \
+    #         --role="roles/run.admin" 2>&1 >/dev/null
+
+    # gcloud projects add-iam-policy-binding "$PROJECT_ID" \
+    #     --member="serviceAccount:${PROJECT_NUMBER}@cloudbuild.gserviceaccount.com" \
+    #     --role="projects/$PROJECT_ID/roles/$CUSTOMROLECB" 2>&1 >/dev/null
+
+    # gcloud projects add-iam-policy-binding "$PROJECT_ID" \
+    #     --member="serviceAccount:${PROJECT_NUMBER}@cloudbuild.gserviceaccount.com" \
+    #     --role="roles/iam.serviceAccountUser"
+
+    # gcloud projects add-iam-policy-binding "$PROJECT_ID" \
+    #     --member="serviceAccount:${PROJECT_NUMBER}@cloudbuild.gserviceaccount.com" \
+    #     --role="roles/cloudbuild.builds.editor"
+
+    USER_EMAIL=$(gcloud config get-value account)
+
+    gcloud projects add-iam-policy-binding "$PROJECT_ID" \
+        --member="user:$USER_EMAIL" \
+        --role="roles/secretmanager.admin"
+
 
     echo "Custom role created and assigned to $SERVICE_ACCOUNT_EMAIL"
 }
 
 
-link_gh_repo(){
+conn_gh_repo(){
     # service-account will need roles   "roles/secretmanager.secrets.setIamPolicy";"roles/secretmanager.secrets.create"
     # Will need the user to authenticate with github 
     CONNECTION_NAME="github_connection"
@@ -150,6 +183,25 @@ link_gh_repo(){
     echo "First choose your google account associated with the cloud project, then log into github where the repo is located."
    
     # Guessing I need to put a loop here to check if it has been properly linked or not
+    # Spinner setup
+    SPINNER=('⠋' '⠙' '⠹' '⠸' '⠼' '⠴' '⠦' '⠧' '⠇' '⠏')
+    i=0
+    MAX_RETRIES=60
+    RETRY=0
+
+    while [[ "$(gcloud builds connections describe "$CONNECTION_NAME" --region="$REGION" --format='value(installationState.stage)')" != "COMPLETE" ]]; do
+        if [[ $RETRY -ge $MAX_RETRIES ]]; then
+            echo -e "\nTimeout waiting for GitHub connection to be authorized."
+            exit 1
+        fi
+
+        printf "\r${SPINNER[$i]} Waiting for authorization... (%d/%d)" "$RETRY" "$MAX_RETRIES"
+        i=$(( (i+1) % ${#SPINNER[@]} ))
+        sleep 1
+        ((RETRY++))
+    done
+
+    echo -e "\rGitHub connection authorized successfully!"
 
     # After this we should create the repo in gcloud
     gcloud beta builds repositories create personal_website \
@@ -159,8 +211,8 @@ link_gh_repo(){
 }
 
 setup_gcrun_trigger(){
-    PN=$(gcloud projects describe ${PROJECT_ID} --format="value(projectNumber)")
-    REPO="tiboeycken-personal_website" # Name of the repo in gcloud -> `gcloud builds repositories list --region=$REGION --connection=$CONNECTION`
+    local PROJECT_NUMBER=$(get_project_number "$PROJECT_ID")
+    local REPO="personal_website" # Name of the repo in gcloud -> `gcloud builds repositories list --region=$REGION --connection=$CONNECTION`
 
     # Correct command - differs from what is stated in the documentation
     gcloud builds triggers create github \
@@ -168,13 +220,15 @@ setup_gcrun_trigger(){
         --repository="projects/$PROJECT_ID/locations/$REGION/connections/$CONNECTION_NAME/repositories/$REPO" \
         --branch-pattern="^main$" \
         --region="$REGION" \
-        --service-account="projects/$PROJECT_ID/serviceAccounts/$PN-compute@developer.gserviceaccount.com" \
+        --service-account="projects/$PROJECT_ID/serviceAccounts/cloud-build-deployer@$PROJECT_ID.iam.gserviceaccount.com" \
         --build-config="cloudbuild.yaml"
+
+    gcloud builds triggers run deploy --region="$REGION" --branch="main"
 }
 
 check_gcloud_installed
 choose_project
 choose_billing_account
 create_and_set_custom_role
-link_gh_repo
+conn_gh_repo
 setup_gcrun_trigger
